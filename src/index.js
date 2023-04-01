509509a14c2c683e77aef9f47b01bff8c1f819f6
RubenBoard <klsj9810@gmail.com>
2023-04-01 21:27:32 +0900

    outline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log(123);
+import express from "express";
+
+const PORT = 7000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`✅ Server listening on http://localhost:${PORT}/ 🚀`);
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "index.html");
+});
+
+app.listen(PORT, handleListening);
